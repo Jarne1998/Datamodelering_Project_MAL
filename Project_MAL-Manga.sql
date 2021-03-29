ddc7679c27e39ca21853397b37ca8812925d9987
Jarne Bauwens <47212105+Jarne1998@users.noreply.github.com>
2021-03-29 14:19:18 +0200

    Toevoegen van Manga script + opvullen van table (nog aanpassen van ID's)

---
+++ Project_MAL-Manga.sql
@@ -0,0 +1,52 @@
+-- GPR LTI d1.1 Jarne Bauwens & Dennis Luyten
+-- Opvullen van Manga table
+
+-- 1)
+SET IDENTITY_INSERT Project_MAL.Manga ON
+INSERT INTO Project_MAL.Manga(mangaId, collectionId, authorId, name, chapters)
+VALUES(1, 1, 1, 'Naruto', 700);
+
+-- 2)
+SET IDENTITY_INSERT Project_MAL.Manga ON
+INSERT INTO Project_MAL.Manga(mangaId, collectionId, authorId, name, chapters)
+VALUES(2, 2, 2, 'Fullmetal Alchemist', 116);
+
+-- 3)
+SET IDENTITY_INSERT Project_MAL.Manga ON
+INSERT INTO Project_MAL.Manga(mangaId, collectionId, authorId, name, chapters)
+VALUES(3, 3, 3, 'Black Clover', 170);
+
+-- 4)
+SET IDENTITY_INSERT Project_MAL.Manga ON
+INSERT INTO Project_MAL.Manga(mangaId, collectionId, authorId, name, chapters)
+VALUES(4, 4, 4, 'Yu-Gi-Oh! 5Ds', 68);
+
+-- 5)
+SET IDENTITY_INSERT Project_MAL.Manga ON
+INSERT INTO Project_MAL.Manga(mangaId, collectionId, authorId, name, chapters)
+VALUES(5, 5, 5, 'Claymore', 159);
+
+-- 6)
+SET IDENTITY_INSERT Project_MAL.Manga ON
+INSERT INTO Project_MAL.Manga(mangaId, collectionId, authorId, name, chapters)
+VALUES(6, 6, 6, 'Bleach', 705);
+
+-- 7)
+SET IDENTITY_INSERT Project_MAL.Manga ON
+INSERT INTO Project_MAL.Manga(mangaId, collectionId, authorId, name, chapters)
+VALUES(7, 7, 7, 'demon Slayer: Kimetsu no Yaiba', 207);
+
+-- 8)
+SET IDENTITY_INSERT Project_MAL.Manga ON
+INSERT INTO Project_MAL.Manga(mangaId, collectionId, authorId, name, chapters)
+VALUES(8, 8, 8, 'Shaman King', 288);
+
+-- 9)
+SET IDENTITY_INSERT Project_MAL.Manga ON
+INSERT INTO Project_MAL.Manga(mangaId, collectionId, authorId, name, chapters)
+VALUES(9, 9, 9, 'Vagabond', 327);
+
+-- 10)
+SET IDENTITY_INSERT Project_MAL.Manga ON
+INSERT INTO Project_MAL.Manga(mangaId, collectionId, authorId, name, chapters)
+VALUES(10, 10, 10, 'Dragon Ball', 520);
