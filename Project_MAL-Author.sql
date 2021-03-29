2d626be4108472e057ff01e81e9bc491d71a10d0
Jarne Bauwens <47212105+Jarne1998@users.noreply.github.com>
2021-03-29 20:15:36 +0200

    Opvullen van gegevens

--- Project_MAL-Author.sql
+++ Project_MAL-Author.sql
@@ -4,49 +4,49 @@
 -- 1)
 SET IDENTITY_INSERT Project_MAL.Author ON
 INSERT INTO Project_MAL.Author(authorId, name, lastname, birthday)
-VALUES();
+VALUES(1, 'Kishimoto', 'Masashi', 1974-11-08);
 
 -- 2)
 SET IDENTITY_INSERT Project_MAL.Author ON
 INSERT INTO Project_MAL.Author(authorId, name, lastname, birthday)
-VALUES();
+VALUES(2, 'Arakawa', 'Hiomu', 1973-05-08);
 
 -- 3)
 SET IDENTITY_INSERT Project_MAL.Author ON
 INSERT INTO Project_MAL.Author(authorId, name, lastname, birthday)
-VALUES();
+VALUES(3, 'Tabata', 'Yuuki', 1984-07-30);
 
 -- 4)
 SET IDENTITY_INSERT Project_MAL.Author ON
 INSERT INTO Project_MAL.Author(authorId, name, lastname, birthday)
-VALUES();
+VALUES(4, 'Hikokubo', 'Masahiro', null);
 
 -- 5)
 SET IDENTITY_INSERT Project_MAL.Author ON
 INSERT INTO Project_MAL.Author(authorId, name, lastname, birthday)
-VALUES();
+VALUES(5, 'Yagi', 'Norihiro', 1968-12-18);
 
 -- 6)
 SET IDENTITY_INSERT Project_MAL.Author ON
 INSERT INTO Project_MAL.Author(authorId, name, lastname, birthday)
-VALUES();
+VALUES(6, 'Kubo', 'Tite', 1977-06-26);
 
 -- 7)
 SET IDENTITY_INSERT Project_MAL.Author ON
 INSERT INTO Project_MAL.Author(authorId, name, lastname, birthday)
-VALUES();
+VALUES(7, 'Gotouge', 'Koyoharu', 1988-05-05);
 
 -- 8)
 SET IDENTITY_INSERT Project_MAL.Author ON
 INSERT INTO Project_MAL.Author(authorId, name, lastname, birthday)
-VALUES();
+VALUES(8, 'Takei', 'Hiryuki', 1972-03-15);
 
 -- 9)
 SET IDENTITY_INSERT Project_MAL.Author ON
 INSERT INTO Project_MAL.Author(authorId, name, lastname, birthday)
-VALUES();
+VALUES(9, 'Inoue', 'takehiko', 1967-06-26);
 
 -- 10)
 SET IDENTITY_INSERT Project_MAL.Author ON
 INSERT INTO Project_MAL.Author(authorId, name, lastname, birthday)
-VALUES();
+VALUES(10, 'Toriyama', 'Akira', 1995-04-05);
