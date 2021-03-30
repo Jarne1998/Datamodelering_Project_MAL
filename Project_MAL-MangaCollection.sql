879e05903efd68a6515cecf8066bf22357992f32
Jarne Bauwens <47212105+Jarne1998@users.noreply.github.com>
2021-03-30 22:07:59 +0200

    Toevoegen van MangaCollection query

---
+++ Project_MAL-MangaCollection.sql
@@ -0,0 +1,52 @@
+-- GPR LTI d1.1 Jarne Bauwens & Dennis Luyten
+-- Opvullen van Manga table
+
+-- 1)
+SET IDENTITY_INSERT Project_MAL.MangaCollection ON
+INSERT INTO Project_MAL.MangaCollection(mangaCollectionId, mangaId, collectionId)
+VALUES(1, 1, 1);
+
+-- 2)
+SET IDENTITY_INSERT Project_MAL.MangaCollection ON
+INSERT INTO Project_MAL.MangaCollection(mangaCollectionId, mangaId, collectionId)
+VALUES(1, 1, 1);
+
+-- 3)
+SET IDENTITY_INSERT Project_MAL.MangaCollection ON
+INSERT INTO Project_MAL.MangaCollection(mangaCollectionId, mangaId, collectionId)
+VALUES(1, 1, 1);
+
+-- 4)
+SET IDENTITY_INSERT Project_MAL.MangaCollection ON
+INSERT INTO Project_MAL.MangaCollection(mangaCollectionId, mangaId, collectionId)
+VALUES(1, 1, 1);
+
+-- 5)
+SET IDENTITY_INSERT Project_MAL.MangaCollection ON
+INSERT INTO Project_MAL.MangaCollection(mangaCollectionId, mangaId, collectionId)
+VALUES(1, 1, 1);
+
+-- 6)
+SET IDENTITY_INSERT Project_MAL.MangaCollection ON
+INSERT INTO Project_MAL.MangaCollection(mangaCollectionId, mangaId, collectionId)
+VALUES(1, 1, 1);
+
+-- 7)
+SET IDENTITY_INSERT Project_MAL.MangaCollection ON
+INSERT INTO Project_MAL.MangaCollection(mangaCollectionId, mangaId, collectionId)
+VALUES(1, 1, 1);
+
+-- 8)
+SET IDENTITY_INSERT Project_MAL.MangaCollection ON
+INSERT INTO Project_MAL.MangaCollection(mangaCollectionId, mangaId, collectionId)
+VALUES(1, 1, 1);
+
+-- 9)
+SET IDENTITY_INSERT Project_MAL.MangaCollection ON
+INSERT INTO Project_MAL.MangaCollection(mangaCollectionId, mangaId, collectionId)
+VALUES(1, 1, 1);
+
+-- 10)
+SET IDENTITY_INSERT Project_MAL.MangaCollection ON
+INSERT INTO Project_MAL.MangaCollection(mangaCollectionId, mangaId, collectionId)
+VALUES(1, 1, 1);
